816866acea3fecb31233fd32acb8a8d5c7d0ea78
Filip Tokarski <filip.tokarski14@gmail.com>
2019-08-20 13:48:07 +0200

    Cleaned up

--- movie_api/urls.py
+++ movie_api/urls.py
@@ -1,13 +1,5 @@
 from .views import MovieViewset, CommentViewset, TopViewset
 from django.urls import path
-from rest_framework.routers import DefaultRouter
-
-# router = DefaultRouter()
-# router.register(r'movies', MovieViewset, basename='movies')
-# router.register(r'comments', CommentViewset, basename='comments')
-# router.register(r'top', TopViewset, basename='top')
-
-# urlpatterns = router.urls
 
 urlpatterns = [
     path('movies', MovieViewset.as_view(), name='movies'),
--- movie_api/utils.py
+++ movie_api/utils.py
@@ -6,11 +6,6 @@ def get_movie(title):
     """Makes request to omdb api and gets data based on given title"""
     BASE_URL = 'http://omdbapi.com/?'
     payload = {'apikey': APIKEY, 't': title}
-    try:
-        request = requests.get(BASE_URL, payload)
-        data = request.json()
-    except:
-        raise H
-        #data = {"Title": "Sorry, there has been an error"}
-
+    request = requests.get(BASE_URL, payload)
+    data = request.json()
     return data
